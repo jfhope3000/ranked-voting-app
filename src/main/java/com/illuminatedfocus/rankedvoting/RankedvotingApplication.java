ef0f1f82152df9380f507323d023bc3447eacf0f
Jack Hope <jfhope3000@gmail.com>
2020-04-29 09:55:52 -0500

    initial add

---
+++ src/main/java/com/illuminatedfocus/rankedvoting/RankedvotingApplication.java
@@ -0,0 +1,13 @@
+package com.illuminatedfocus.rankedvoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankedvotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankedvotingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/illuminatedfocus/rankedvoting/RankedvotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.illuminatedfocus.rankedvoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankedvotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
